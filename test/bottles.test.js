1838221450e9061eea88d98e64d7550cec676e5b
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2020-09-22 20:12:57 +0200

    verse 0

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -37,7 +37,7 @@ describe('Bottles', () => {
     expect(new Bottles().verse(1)).toBe(expected);
   });
 
-  test.skip('verse 0', () => {
+  test('verse 0', () => {
     const expected =
       'No more bottles of milk on the wall, ' +
       'no more bottles of milk.\n' +
